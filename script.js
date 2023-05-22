eec9bd17655d4b5e000a1d315c13245cc97d04a9
PC Casa <guilhermegalvao925@gmail.com>
2023-05-21 22:35:07 -0400

    Implementações do projeto

---
+++ script.js
@@ -0,0 +1,6 @@
+var img1 = "images/Dell_Q3FY22_Microsite_Alienware-M15-R5-03.png";
+var img2 = "images/laptop-alienware-m15-r7-gallery-7-br.avif";
+var img3 = "images/laptop-alienware-m15-r7-gallery-hero-br-2.avif";
+function Seleciona(){
+
+}
