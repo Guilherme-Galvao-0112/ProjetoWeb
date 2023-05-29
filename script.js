a9a2a8a133e31e0ae682d93721ec8c95695e7380
PC Casa <guilhermegalvao925@gmail.com>
2023-05-28 21:21:18 -0400

    segunda tela carrinho

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-var img1 = "images/Dell_Q3FY22_Microsite_Alienware-M15-R5-03.png";
-var img2 = "images/laptop-alienware-m15-r7-gallery-7-br.avif";
-var img3 = "images/laptop-alienware-m15-r7-gallery-hero-br-2.avif";
-function Seleciona(){
-
+function img01(){
+    document.getElementById("imgPrincipal").src="images/Dell_Q3FY22_Microsite_Alienware-M15-R5-03.png"
+};
+function img02(){
+    document.getElementById("imgPrincipal").src="images/laptop-alienware-m15-r7-gallery-7-br.avif"
 }
+function img03(){
+    document.getElementById("imgPrincipal").src="images/laptop-alienware-m15-r7-gallery-hero-br-2.avif"
+}
+function img04(){
+    document.getElementById("imgPrincipal").src="images/laptop-alienware-m15-r7-gallery-hero-br.avif"
+}
+function img05(){
+    document.getElementById("imgPrincipal").src="images/notebook-alienware-m15-r7-gallery-2-pt-br.avif"
+}
+
---
+++ script2.js
@@ -0,0 +1,6 @@
+var qtd = 0;
+function plus() {
+    qtd = qtd + 1;
+    console.log(qtd)
+    document.getElementsByClassName("qtd").value = qtd;
+}
