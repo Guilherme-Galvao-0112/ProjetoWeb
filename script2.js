7576bc38013197cbdd9b19a9892ea28f4fa924b0
PC Casa <guilhermegalvao925@gmail.com>
2023-05-29 18:23:30 -0400

    alterações

--- script2.js
+++ script2.js
@@ -1,6 +1,20 @@
-var qtd = 0;
-function plus() {
+var qtd = parseInt(document.getElementsByClassName('qtd'));
+
+function plus(){
     qtd = qtd + 1;
-    console.log(qtd)
-    document.getElementsByClassName("qtd").value = qtd;
+    console.log(qtd);
+    atualiza();
+}
+function minus(){
+    qtd = qtd -1;
+    console.log(qtd);
+    atualiza();
+}
+
+
+function atualiza(){
+    var Vtotal = 16099 * qtd;
+    document.getElementsByClassName('Tprice').innerHTML = Vtotal;
+    document.getElementsByClassName('qtd').value=qtd; 
+    console.log(Vtotal,qtd);
 }
